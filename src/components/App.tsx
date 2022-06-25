e8a85a4f919f209970a8e8081e4139dc21dbad37
Quantum Snowball <quantum.snowball@gmail.com>
2022-06-26 02:03:27 +0800

    feature: basic fetch and display of a random quote

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/App.css';
 
+//@ts-ignore
+async function fetchQuotes(setContent, setAuthor) {
+  const url = 'https://api.quotable.io/random'
+  const quote = await fetch(url).then(resp => resp.json())
+  setContent(quote.content)
+  setAuthor("-- " + quote.author)
+}
+
 function App() {
+  const [content, setContent] = useState("")
+  const [author, setAuthor] = useState("")
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          The world is beautiful
-        </p>
-        <code>-- Peter</code>
+        <p>{content}</p>
+        <code>{author}</code>
+        <button className="fetch" onClick={() => fetchQuotes(setContent, setAuthor)}>GET</button>
       </header>
-    </div>
+    </div >
   );
 }
 
 export default App;
+
