8c1ccccee1216e33f3bcd0211d0cfebfbca802ed
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-20 01:02:07 +0800

    cleanup: rename service worker files

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import App from './components/App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import * as serviceWorkerRegistration from './swReg'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
