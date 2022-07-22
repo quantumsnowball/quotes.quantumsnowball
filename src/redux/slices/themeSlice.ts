0576a447fb04b2a04a92f93411c41f3171c858d8
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-23 02:08:39 +0800

    step: create the themeSlice

---
+++ src/redux/slices/themeSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { ColorMode } from '../../types'
+
+
+interface ThemeState {
+  mode: ColorMode
+}
+
+const initialState: ThemeState = {
+  mode: 'dark'
+}
+
+export const themeSlice = createSlice({
+  name: 'theme',
+  initialState,
+  reducers: {
+    toggleMode: state => {
+      state.mode = state.mode
+    }
+  }
+})
+
+export const { toggleMode } = themeSlice.actions
+
+export default themeSlice.reducer
