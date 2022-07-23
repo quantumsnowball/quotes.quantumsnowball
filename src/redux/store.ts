0e7c0f65f3d33eeb367adc2f22aedd9e9cde26d4
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-23 19:34:53 +0800

    refactor: export RootState type more directly, favoring auto-completion to work

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -30,6 +30,6 @@ export const store = configureStore({
 export const persistor = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof rootReducer>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
