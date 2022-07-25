3caa36d6f6a1a3944bf1ac893bf97acfbba7cdcf
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-25 19:56:08 +0800

    refactor: group mui imports

--- src/components/MenuBar/MenuDrawer.tsx
+++ src/components/MenuBar/MenuDrawer.tsx
@@ -1,11 +1,8 @@
-import Box from '@mui/material/Box'
-import SwipeableDrawer from '@mui/material/SwipeableDrawer'
-import List from '@mui/material/List'
-import Divider from '@mui/material/Divider'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
+import {
+  Box, Divider,
+  List, ListItem, ListItemButton, ListItemIcon, ListItemText,
+  SwipeableDrawer
+} from '@mui/material'
 import InboxIcon from '@mui/icons-material/MoveToInbox'
 import ColorLensIcon from '@mui/icons-material/ColorLens'
 
